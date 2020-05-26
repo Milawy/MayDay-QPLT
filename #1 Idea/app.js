08df6c370991d7f32f0ef39261e932ab82116c9c
Milawy <39126775+Milawy@users.noreply.github.com>
2020-05-26 11:57:15 +0200

    Added redirects to other html pages

--- #1 Idea/app.js
+++ #1 Idea/app.js
@@ -8,3 +8,23 @@ function closeTabs() {
     document.getElementById("side-bar").style.display = "none";
     document.getElementById("side-bar-button").style.display = "flex";
 }
+
+function openHome() {
+    window.location.href = "display.html";
+}
+
+function openWeather() {
+    window.location.href = "weather.html";
+}
+
+function openCovid() {
+    window.location.href = "covid.html";
+}
+
+function openTrends() {
+    window.location.href = "trends.html";
+}
+
+function openTransport() {
+    window.location.href = "transport.html";
+}
