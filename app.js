c5a825a7510f6bd40b3537e8da45d2777b6e9a6c
Milawy <39126775+Milawy@users.noreply.github.com>
2020-05-26 11:12:30 +0200

    New style

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ function openTabs() {
     document.getElementById("side-bar").style.flexDirection = "column";
     document.getElementById("side-bar-button").style.display = 'none';
 }
+
 function closeTabs() {
     document.getElementById("side-bar").style.display = "none";
     document.getElementById("side-bar-button").style.display = "flex";
