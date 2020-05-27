61386afb203566261b04273af07db9aef7dad220
Milawy <39126775+Milawy@users.noreply.github.com>
2020-05-27 10:42:46 +0200

    Decided the style + Structured directories

---
+++ js/app.js
@@ -0,0 +1,19 @@
+function openHome() {
+    window.location.href = "../structures/home.html";
+}
+
+function openWeather() {
+    window.location.href = "../structures/weather.html";
+}
+
+function openCovid() {
+    window.location.href = "../structures/covid.html";
+}
+
+function openTrends() {
+    window.location.href = "../structures/trends.html";
+}
+
+function openTransport() {
+    window.location.href = "../structures/transport.html";
+}
