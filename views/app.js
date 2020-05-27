0dac646422ba56fd33a7204b11f810aa8f1aee6d
Remi <remi.arbache@isen.yncrea.fr>
2020-05-27 11:28:28 +0200

    Refactoring structure

---
+++ views/app.js
@@ -0,0 +1,19 @@
+function openHome() {
+    window.location.href = "home.html";
+}
+
+function openWeather() {
+    window.location.href = "weather.html";
+}
+
+function openCovid() {
+    window.location.href = "covid.html";
+}
+
+function openTrends() {
+    window.location.href = "trends.html";
+}
+
+function openTransport() {
+    window.location.href = "transport.html";
+}
